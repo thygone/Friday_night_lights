cab81a9b24b1c92e72f795823af2129cdb4f9d3e
Melanie Monkurai <82848743+Melaniemonks@users.noreply.github.com>
2021-12-11 22:01:15 -0700

    Add files via upload

---
+++ ResultActivity.kt
@@ -0,0 +1,17 @@
+package com.example.newproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+    lateinit var opt1: Button
+
+
+
+}
